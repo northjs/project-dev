edbdb22b92e9b74d244b38339156d6502b3be972
alexeyc <995719515@qq.com>
2017-08-24 23:21:33 +0800

    工程构建

---
+++ p2p-sso/p2p-web-sso/src/main/java/com/hto56/sso/P2pWebSsoApplication.java
@@ -0,0 +1,13 @@
+package com.hto56.sso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class P2pWebSsoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(P2pWebSsoApplication.class, args);
+	}
+	
+}
